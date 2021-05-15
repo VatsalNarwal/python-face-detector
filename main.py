69def3970e15b2c81f79870ad859e1f75772e087
Vatsal-Narwal <narwal.vatsal@gmail.com>
2021-05-15 18:18:37 +0530

    Commited

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+import cv2
+from random import randrange
+
+# Load trained data from xml file
+td = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read image from file
+img = cv2.resize(cv2.imread('_data/em.png'), (600, 540))
+
+# GrayScale the image
+grayScaleImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Get face cordinates
+faceCordinates = td.detectMultiScale(grayScaleImg)
+
+print(f"Face Cordinates ➜ {faceCordinates[0]}")
+
+(x, y, w, h) = faceCordinates[0]
+
+cv2.rectangle(img, (x, y), (x + w, y + h),
+              (randrange(256), randrange(256), randrange(256)), 2)
+
+# Show up the image
+cv2.imshow("Face Detector", img)
+
+# Wait for a key to be pressed
+cv2.waitKey()
+
+sys.exit("Process completed!")
---
+++ webcam.py
@@ -0,0 +1,36 @@
+import sys
+import cv2
+from random import randrange
+
+# Load trained data from xml file
+td = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+wc = cv2.VideoCapture(0)
+
+color = (randrange(256), randrange(256), randrange(256))
+
+while True:
+    successful_frame_read, frame = wc.read()
+
+    # GrayScale the frame
+    grayScaleImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Get face cordinates
+    faceCordinates = td.detectMultiScale(grayScaleImg)
+
+
+    for (x, y, w, h) in faceCordinates:
+
+        print(f"Face Cordinates ➜ {faceCordinates[0]}")
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+    # Show up the image
+    cv2.imshow("Face Detector", frame)
+
+    cv2.waitKey(1)
+
+# Wait for a key to be pressed
+cv2.waitKey()
+
+sys.exit("Process completed!")
